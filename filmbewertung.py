83b27da70fc8d78c1f86cfa3ae8cbb9cc7ed6831
Marcus Wunderle <102217370+WunderleMarcus@users.noreply.github.com>
2023-10-30 11:16:25 +0100

    Update filmbewertung.py

--- filmbewertung.py
+++ filmbewertung.py
@@ -12,14 +12,20 @@ film_bewertungen = {
 
 # 3. Funktion für durchschnittliche Bewertung
 def durchschnittliche_bewertung(wörterbuch):
+    # Die Funktion nimmt ein Wörterbuch als Argument und berechnet den Durchschnitt
+    # der Werte (Bewertungen) in diesem Wörterbuch.
     return sum(wörterbuch.values()) / len(wörterbuch)
 
 # 4. Funktion für den Film mit der höchsten Bewertung
 def höchste_bewertung(wörterbuch):
+    # Die Funktion nimmt ein Wörterbuch als Argument und gibt den Schlüssel (Filmtitel)
+    # mit dem höchsten Wert (Bewertung) im Wörterbuch zurück.
     return max(wörterbuch, key=wörterbuch.get)
 
 # 5. Funktion für Filme mit Bewertung von 6 oder höher
 def filme_ab_6(wörterbuch):
+    # Die Funktion nimmt ein Wörterbuch als Argument und erstellt eine Liste der Filmtitel,
+    # deren Bewertung 6 oder höher ist.
     return [film for film, bewertung in wörterbuch.items() if bewertung >= 6]
 
 # 6. Einen weiteren Film hinzufügen
